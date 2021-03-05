5d1bfcedeb9baabab4a205b452e87020ed8a49bb
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2021-03-05 15:54:22 -0300

    Show projects in a grid

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 import {
   Typography,
-  List,
+  Container,
   makeStyles,
+  Grid,
   Card,
   CardContent,
   CardMedia,
@@ -9,8 +10,21 @@ import {
 } from "@material-ui/core";
 
 const useStyles = makeStyles({
+  title: {
+    paddingTop: "20px",
+  },
+  description: {
+    paddingTop: "20px",
+    paddingBottom: "20px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
   root: {
+    height: "100%",
+  },
+  card: {
     display: "flex",
+    height: "100%",
   },
   details: {
     display: "flex",
@@ -20,7 +34,11 @@ const useStyles = makeStyles({
   linkText: {
     textDecoration: `none`,
   },
-  cover: {
+  imageContainer: {
+    marginTop: "auto",
+    marginBottom: "auto",
+  },
+  image: {
     height: 150,
     width: 150,
     objectFit: "contain",
@@ -40,7 +58,7 @@ const projects = [
     image: "svg-identicon-generator.png",
   },
   {
-    name: "Visual representation for some fuzzy logic operators",
+    name: "Visual representation for a few fuzzy logic operators",
     url: "https://github.com/danielwohlgemuth/fuzzy-operators-visualization",
     image: "fuzzy-logic.png",
   },
@@ -51,29 +69,46 @@ function Home() {
 
   return (
     <>
-      <Typography variant="h4">Projects</Typography>
+      <Typography variant="h4" align="center" className={classes.title}>
+        Projects
+      </Typography>
 
-      <List>
-        {projects.map((project) => (
-          <Box p={1} key={project.url}>
-            <a href={project.url} className={classes.linkText}>
-              <Card className={classes.root}>
-                <CardMedia
-                  className={classes.cover}
-                  image={"/images/" + project.image}
-                  component="img"
-                  title={project.name}
-                />
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <Typography variant="subtitle1">{project.name}</Typography>
-                  </CardContent>
-                </div>
-              </Card>
-            </a>
-          </Box>
-        ))}
-      </List>
+      <Typography
+        variant="body1"
+        align="center"
+        className={classes.description}
+      >
+        Different projects that I've been working on to try out ideas or
+        technologies.
+      </Typography>
+
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          {projects.map((project) => (
+            <Grid item xs={12} md={6} lg={4} key={project.url}>
+              <Box p={1} className={classes.root}>
+                <a href={project.url} className={classes.linkText}>
+                  <Card className={classes.card} raised={true}>
+                    <div className={classes.imageContainer}>
+                      <CardMedia
+                        className={classes.image}
+                        image={"/images/" + project.image}
+                        component="img"
+                        title={project.name}
+                      />
+                    </div>
+                    <div className={classes.details}>
+                      <CardContent className={classes.content}>
+                        <Typography variant="h6">{project.name}</Typography>
+                      </CardContent>
+                    </div>
+                  </Card>
+                </a>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 }
