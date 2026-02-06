5c5a94d5d655ec9f94365a8d5318b14959667195
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2026-02-06 08:42:43 -0300

    Update About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,14 +10,14 @@ function About() {
           Hi, I'm Daniel Wohlgemuth.
         </Typography>
         <Typography sx={{ typography: 'body1', lineHeight: 2 }}>
-          I'm a software engineer with a special interest in backend development and DevOps practices.
+          I'm a full-stack developer, leaning towards backend development and DevOps, with decent frontend skills. I use this site to share some of my more interesting projects and mini-apps.
         </Typography>
         <Typography sx={{ typography: 'body1', lineHeight: 2 }}>
           You can find me on{" "}
           <Link href="https://www.linkedin.com/in/danielwohlgemuth/" target="_blank" rel="noreferrer">
             LinkedIn
           </Link>{" "}
-          or see some of my projects on{" "}
+          or browse my repos on{" "}
           <Link href="https://github.com/danielwohlgemuth" target="_blank" rel="noreferrer">
             GitHub
           </Link>.
